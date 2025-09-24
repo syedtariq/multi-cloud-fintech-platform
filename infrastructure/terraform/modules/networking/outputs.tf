3c12c4c3809b07593e098de6b96775335d42f7ee
syedtariq <tariqhassanin@yahoo.co.in>
2025-09-24 21:10:05 +0530

    Auto failover  and in time DB synch

--- infrastructure/terraform/modules/networking/outputs.tf
+++ infrastructure/terraform/modules/networking/outputs.tf
@@ -27,3 +27,13 @@ output "nat_gateway_ids" {
   description = "IDs of the NAT Gateways"
   value       = aws_nat_gateway.main[*].id
 }
+
+output "private_route_table_ids" {
+  description = "IDs of the private route tables"
+  value       = aws_route_table.private[*].id
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
